7a8b4f0cb5485ee8eee49ed1678cdf4b05cb2ca2
fromwindowstolinux <43229477+fromwindowstolinux@users.noreply.github.com>
2020-01-23 00:20:43 +0800

    Add files via upload

---
+++ Topic_1/Example_1-4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    // int a = 0;
+    // x[a] = 5;
+    // x[a <=> 0] = 5;
+    // x[0] = 5;
+
+    int x[0];
+    x[0] = 5;
+
+    printf("index 0 = digit %d\n", x[0]);
+}
